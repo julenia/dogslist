bcd77630154e43f4d6df22b07084d2be5d0f036c
julenia <julia.jankowska@onet.eu>
2019-07-17 12:34:53 +0200

    creates a list of dogs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './App.css';
+import DogsList from './components/DogsList'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <DogsList></DogsList>
+      </main>
+    </div>
+  );
+}
+
+export default App;
